a8b3dd649929a6100cfeb073bd04ae7d4cf8ed99
Igues Lacerda <igues@inlive.com.br>
2024-10-21 11:16:23 -0300

    bug em arquivo MenuTop.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import Card from "./componentes/Card";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h4>Web site experiment</h4>
-      </header>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-        <p style={{ width: "100%" }}>Corpo do site. Experimento de layout.</p>
-        <Card titulo="Titulo do card">
-          <p>
-            Primeira linha
-            <br />
-            Conteúdo do card.
-            <br />
-            Pode ser multi linhas.
-          </p>
-          <div>
-            E vários elementos &nbsp;<button>Ok</button>
-          </div>
-        </Card>
-        <Card
-          titulo="Customizado"
-          maxWidth="150px"
-          backgroundColor="lightgoldenrodyellow"
-        >
-          A largura e cor de fundo pode ser customizado.
-        </Card>
+      <div className="content" style={{ padding: "0 20px 20px 20px" }}>
+        <header className="App-header">
+          <h4>Web site experiment</h4>
+        </header>
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+          <p style={{ width: "100%" }}>Corpo do site. Experimento de layout.</p>
+          <Card titulo="Titulo do card">
+            <p>
+              Primeira linha
+              <br />
+              Conteúdo do card.
+              <br />
+              Pode ser multi linhas.
+            </p>
+            <div>
+              E vários elementos &nbsp;<button>Ok</button>
+            </div>
+          </Card>
+          <Card
+            titulo="Customizado"
+            maxWidth="150px"
+            backgroundColor="lightgoldenrodyellow"
+          >
+            A largura e cor de fundo pode ser customizado.
+          </Card>
+        </div>
       </div>
     </div>
   );
